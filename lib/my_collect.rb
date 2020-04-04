bc61e833e8db1e0d9f0a1f85540f96cd4a66cee1
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-04-04 15:53:53 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,11 @@
 
 def my_collect(a_collection)
 counter = 0 
-new_array = []
+  new_array = []
 while counter < a_collection.length 
-new_array << yield(a_collection[counter])
+    new_array << yield(a_collection[counter])
 counter += 1
+end
+  new_array
 end
 
